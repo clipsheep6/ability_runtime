c12b37cab366f5d4ff80dff654649c27c6c42881
zhoujun62 <zhoujun62@huawei.com>
2022-01-05 16:58:53 +0800

    auto start settings data ability
    Signed-off-by: zhoujun62 <zhoujun62@huawei.com>

--- services/abilitymgr/include/ability_config.h
+++ services/abilitymgr/include/ability_config.h
@@ -47,7 +47,7 @@ const std::string FLOATING_WINDOW_PERMISSION = "ohos.permission.SYSTEM_FLOAT_WIN
 const std::string LAUNCHER_ABILITY_NAME = "com.ohos.launcher.MainAbility";
 const std::string LAUNCHER_BUNDLE_NAME = "com.ohos.launcher";
 
-const std::string SETTINGS_DATA_ABILITY_NAME = "com.ohos.settingsdata.DataAbility";
+const std::string SETTINGS_DATA_ABILITY_NAME = "com.ohos.settingsdata.data";
 const std::string SETTINGS_DATA_BUNDLE_NAME = "com.ohos.settingsdata";
 
 const std::string APPLICATION_SELECTOR_BUNDLE_NAME = "com.ohos.selector";
