ab9aee2b413bdc1861d44dba4e0a3de5ffa36a05
altay <yuaqiang1@huawei.com>
2022-01-26 15:25:27 +0800

    Description:add code to weekly Sig:SIG_ApplicationFramework Feature or Bugfix:Feature Binary Source:No
    Signed-off-by: altay <yuaqiang1@huawei.com>
    Change-Id: I2a785848be51f7ab8fba6395419f873f8279fb89

--- frameworks/kits/ability/native/src/ability_runtime/js_ability_context.cpp
+++ frameworks/kits/ability/native/src/ability_runtime/js_ability_context.cpp
@@ -38,7 +38,6 @@ constexpr int32_t INDEX_TWO = 2;
 constexpr size_t ARGC_ZERO = 0;
 constexpr size_t ARGC_ONE = 1;
 constexpr size_t ARGC_TWO = 2;
-constexpr size_t ARGC_THREE = 3;
 constexpr int32_t ERROR_CODE_ONE = 1;
 
 void JsAbilityContext::Finalizer(NativeEngine* engine, void* data, void* hint)
