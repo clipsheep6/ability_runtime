ad1ad0900bbeb690df1162497ed175803fb3601f
zmx <1029211721@qq.com>
2022-02-11 16:07:56 +0800

    distributed permission check
    Signed-off-by: zmx <1029211721@qq.com>
    Change-Id: I9141f1788ee6ef9a2eb56db93a7713eafa56dd8d

--- frameworks/kits/ability/native/src/distributed_ability_runtime/distributed_client.cpp
+++ frameworks/kits/ability/native/src/distributed_ability_runtime/distributed_client.cpp
@@ -37,7 +37,7 @@ sptr<IRemoteObject> DistributedClient::GetDmsProxy()
 }
 
 int32_t DistributedClient::StartRemoteAbility(const OHOS::AAFwk::Want& want,
-    int32_t callerUid, int32_t requestCode)
+    int32_t callerUid, int32_t requestCode, uint32_t tokenCaller)
 {
     HILOG_INFO("called");
     sptr<IRemoteObject> remote = GetDmsProxy();
@@ -51,12 +51,13 @@ int32_t DistributedClient::StartRemoteAbility(const OHOS::AAFwk::Want& want,
     PARCEL_WRITE_HELPER(data, Parcelable, &want);
     PARCEL_WRITE_HELPER(data, Int32, callerUid);
     PARCEL_WRITE_HELPER(data, Int32, requestCode);
+	PARCEL_WRITE_HELPER(data, Uint32, tokenCaller);
     MessageParcel reply;
     PARCEL_TRANSACT_SYNC_RET_INT(remote, START_REMOTE_ABILITY, data, reply);
 }
 
-int32_t DistributedClient::ConnectRemoteAbility(const OHOS::AAFwk::Want& want,
-    const sptr<IRemoteObject>& connect, int32_t callerUid, int32_t callerPid)
+int32_t DistributedClient::ConnectRemoteAbility(const OHOS::AAFwk::Want& want, const sptr<IRemoteObject>& connect,
+    int32_t callerUid, int32_t callerPid, uint32_t tokenCaller)
 {
     HILOG_INFO("called");
     if (connect == nullptr) {
@@ -77,6 +78,7 @@ int32_t DistributedClient::ConnectRemoteAbility(const OHOS::AAFwk::Want& want,
     PARCEL_WRITE_HELPER(data, RemoteObject, connect);
     PARCEL_WRITE_HELPER(data, Int32, callerUid);
     PARCEL_WRITE_HELPER(data, Int32, callerPid);
+    PARCEL_WRITE_HELPER(data, Uint32, tokenCaller);
     MessageParcel reply;
     PARCEL_TRANSACT_SYNC_RET_INT(remote, CONNECT_REMOTE_ABILITY, data, reply);
 }
