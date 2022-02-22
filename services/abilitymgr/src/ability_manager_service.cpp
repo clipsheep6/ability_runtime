2f6de0c9abdb4766562481d3feb498f68c165484
unknown <sijunjie@huawei.com>
2022-02-22 11:06:14 +0800

    add multi-user
    Signed-off-by: unknown <sijunjie@huawei.com>

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -1647,7 +1647,7 @@ sptr<IAbilityScheduler> AbilityManagerService::AcquireDataAbility(
 
     std::shared_ptr<DataAbilityManager> dataAbilityManager = GetDataAbilityManagerByUserId(userId);
     CHECK_POINTER_AND_RETURN(dataAbilityManager, nullptr);
-    return dataAbilityManage->Acquire(abilityRequest, tryBind, callerToken, isSystem);
+    return dataAbilityManager->Acquire(abilityRequest, tryBind, callerToken, isSystem);
 }
 
 bool AbilityManagerService::CheckDataAbilityRequest(AbilityRequest &abilityRequest)
