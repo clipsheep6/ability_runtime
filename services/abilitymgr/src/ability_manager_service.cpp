0fd3922741ce4c64ac12ddf6dc802f41751366a9
xuchenghua <xuchenghua09@huawei.com>
2022-02-22 15:53:39 +0000

    xuchenghua09@huawei.com
    Signed-off-by: xuchenghua <xuchenghua09@huawei.com>

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -3447,10 +3447,12 @@ void AbilityManagerService::StartSystemApplication()
 
     if (!amsConfigResolver_ || amsConfigResolver_->NonConfigFile()) {
         HILOG_INFO("start all");
+        StartingSettingsDataAbility();
         StartingSystemUiAbility();
         return;
     }
 
+    StartingSettingsDataAbility();
     StartingSystemUiAbility();
 
     StartupResidentProcess();
@@ -4026,7 +4028,6 @@ void AbilityManagerService::StartSystemAbilityByUser(int32_t userId)
     if (!amsConfigResolver_ || amsConfigResolver_->NonConfigFile()) {
         HILOG_INFO("start all");
         StartingLauncherAbility();
-        StartingSettingsDataAbility();
         StartingScreenLockAbility();
         return;
     }
@@ -4034,11 +4035,6 @@ void AbilityManagerService::StartSystemAbilityByUser(int32_t userId)
     if (amsConfigResolver_->GetStartLauncherState()) {
         HILOG_INFO("start launcher");
         StartingLauncherAbility();
-    }
-
-    if (amsConfigResolver_->GetStartSettingsDataState()) {
-        HILOG_INFO("start settingsdata");
-        StartingSettingsDataAbility();
     }
 
     if (amsConfigResolver_->GetStartScreenLockState()) {
