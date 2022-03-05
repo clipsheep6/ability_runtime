dcabfba4101b55c85ea73c15977ec1241898b56e
sin_dzzeng <13277932042@163.com>
2022-03-05 20:12:18 +0800

    :用户程序框架UI解耦
    Signed-off-by: sin_dzzeng <13277932042@163.com>

--- frameworks/kits/ability/native/src/ability_runtime/js_ability.cpp
+++ frameworks/kits/ability/native/src/ability_runtime/js_ability.cpp
@@ -26,15 +26,18 @@
 #include "js_runtime.h"
 #include "js_runtime_utils.h"
 #include "napi_common_configuration.h"
+#ifdef SUPPORT_GRAPHICS
 #include "js_window_stage.h"
+#endif
 #include "napi_common_want.h"
 #include "napi_remote_object.h"
 #include "string_wrapper.h"
 
 namespace OHOS {
 namespace AbilityRuntime {
+#ifdef SUPPORT_GRAPHICS
 const std::string PAGE_STACK_PROPERTY_NAME = "pageStack";
-
+#endif
 Ability *JsAbility::Create(const std::unique_ptr<Runtime> &runtime)
 {
     return new JsAbility(static_cast<JsRuntime &>(*runtime));
@@ -172,6 +175,7 @@ void JsAbility::OnStop()
     }
 }
 
+#ifdef SUPPORT_GRAPHICS
 void JsAbility::OnSceneCreated()
 {
     Ability::OnSceneCreated();
@@ -263,6 +267,7 @@ void JsAbility::OnBackground()
         delegator->PostPerformBackground(CreateADelegatorAbilityProperty());
     }
 }
+#endif
 
 bool JsAbility::OnContinue(WantParams &wantParams)
 {
@@ -447,6 +452,7 @@ void JsAbility::CallObjectMethod(const char *name, NativeValue *const *argv, siz
     nativeEngine.CallFunction(value, methodOnCreate, argv, argc);
 }
 
+#ifdef SUPPORT_GRAPHICS
 std::unique_ptr<NativeReference> JsAbility::CreateAppWindowStage()
 {
     HandleScope handleScope(jsRuntime_);
@@ -539,6 +545,7 @@ void JsAbility::DoOnForeground(const Want &want)
     scene_->GoForeground(Ability::sceneFlag_);
     HILOG_INFO("%{public}s end scene_->GoForeground.", __func__);
 }
+#endif
 
 std::shared_ptr<AppExecFwk::ADelegatorAbilityProperty> JsAbility::CreateADelegatorAbilityProperty()
 {
@@ -550,6 +557,7 @@ std::shared_ptr<AppExecFwk::ADelegatorAbilityProperty> JsAbility::CreateADelegat
     return property;
 }
 
+#ifdef SUPPORT_GRAPHICS
 void JsAbility::RequsetFocus(const Want &want)
 {
     HILOG_INFO("%{public}s called.", __func__);
@@ -565,5 +573,6 @@ void JsAbility::RequsetFocus(const Want &want)
     }
     scene_->GoForeground(Ability::sceneFlag_);
 }
+#endif
 }  // namespace AbilityRuntime
 }  // namespace OHOS
