75517933a2779cf5ce7260ecdc19203bdc02908b
zhoushicheng2@huawei.com <zhoushicheng2@huawei.com>
2022-05-15 17:08:28 +0800

    zhoushicheng2@huawei.com
    Signed-off-by: zhoushicheng2@huawei.com <zhoushicheng2@huawei.com>
    Change-Id: I42ea84770774b68477b14058ebc2e12875333291

--- services/abilitymgr/src/ability_record.cpp
+++ services/abilitymgr/src/ability_record.cpp
@@ -261,6 +261,7 @@ void AbilityRecord::ProcessForegroundAbility(uint32_t sceneFlag)
 
 void AbilityRecord::BackgroundAbility(const Closure &task)
 {
+    BYTRACE_NAME(BYTRACE_TAG_ABILITY_MANAGER, __PRETTY_FUNCTION__);
     HILOG_INFO("Move the ability to background, ability:%{public}s.", abilityInfo_.name.c_str());
     if (lifecycleDeal_ == nullptr) {
         HILOG_ERROR("Move the ability to background fail, lifecycleDeal_ is null.");
@@ -303,6 +304,7 @@ void AbilityRecord::BackgroundAbility(const Closure &task)
 
 int AbilityRecord::TerminateAbility()
 {
+    BYTRACE_NAME(BYTRACE_TAG_ABILITY_MANAGER, __PRETTY_FUNCTION__);
     HILOG_INFO("Schedule terminate ability to AppMs, ability:%{public}s.", abilityInfo_.name.c_str());
     AppExecFwk::RunningProcessInfo processInfo = {};
     DelayedSingleton<AppScheduler>::GetInstance()->GetRunningProcessInfoByToken(token_, processInfo);
