74520b9478e9311839f46ff4f11e006cc89b3b60
hwyml <11734288+hwyml@user.noreply.gitee.com>
2022-12-22 12:14:58 +0000

    update frameworks/native/appkit/ability_runtime/context/context_impl.cpp.

--- frameworks/native/appkit/ability_runtime/context/context_impl.cpp
+++ frameworks/native/appkit/ability_runtime/context/context_impl.cpp
@@ -368,7 +368,7 @@ void ContextImpl::InitResourceManager(const AppExecFwk::BundleInfo &bundleInfo,
             }
             if (currentBundle) {
                 loadPath = std::regex_replace(loadPath, inner_pattern, LOCAL_CODE_PATH);
-            } else if (bundleInfo.name == "com.hw.thirdpartysdkdemo") { // TODO: 判断bundle是否为library
+            } else if (bundleInfo.applicationInfo.bundleName == "com.hw.thirdpartysdkdemo") { // TODO: 判断bundle是否为library
                 loadPath = std::regex_replace(loadPath, outer_pattern, LOCAL_CODE_PATH);
             } else {
                 loadPath = std::regex_replace(loadPath, outer_pattern, LOCAL_BUNDLES);
