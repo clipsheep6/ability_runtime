bee57906ea6c5063bc77c7fbcad883f148bc1419
x00678224 <xingzejian3@huawei.com>
2023-04-26 15:40:21 +0800

    make pixelmap to be purgeable
    Signed-off-by: x00678224 <xingzejian3@huawei.com>
    Change-Id: I95a4c64ab17829011ef01d7fdaa9067b35f02990

--- frameworks/native/appkit/app/main_thread.cpp
+++ frameworks/native/appkit/app/main_thread.cpp
@@ -68,6 +68,10 @@
 #include "nweb_helper.h"
 #endif
 
+#ifdef IMAGE_PURGEABLE_PIXELMAP
+#include "purgeable_resource_manager.h"
+#endif
+
 #if defined(ABILITY_LIBRARY_LOADER) || defined(APPLICATION_LIBRARY_LOADER)
 #include <dirent.h>
 #include <dlfcn.h>
@@ -1785,6 +1789,11 @@ void MainThread::HandleForegroundApplication()
         HILOG_ERROR("MainThread::handleForegroundApplication error!");
         return;
     }
+
+#ifdef IMAGE_PURGEABLE_PIXELMAP
+    HILOG_DEBUG("[PurgeableResourceManager] Ability OnForeground ExecBeginVisitPurgeableMem.");
+    PurgeableMem::PurgeableResourceManager::GetInstance().ExecBeginVisitPurgeableMem();
+#endif
 
     if (!applicationImpl_->PerformForeground()) {
         HILOG_ERROR("MainThread::handleForegroundApplication error!, applicationImpl_->PerformForeground() failed");
