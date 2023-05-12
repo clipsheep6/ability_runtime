a5e1873173d537fb719121229dd27d5d51419976
wangzhen <wangzhen346@huawei.com>
2023-05-12 11:50:11 +0800

    local change

--- js_environment/frameworks/js_environment/src/js_environment.cpp
+++ js_environment/frameworks/js_environment/src/js_environment.cpp
@@ -15,7 +15,8 @@
 
 #include "js_environment.h"
 
-#include "js_env_logger.h"
+#define JSENV_LOG_D(param) ; // #include "js_env_logger.h"
+#define JSENV_LOG_E(param) ;
 #include "js_environment_impl.h"
 #include "native_engine/impl/ark/ark_native_engine.h"
 #include "uncaught_exception_callback.h"
