25af3f9daa86f3f4c2f0fb1421e144eca9dd3cf7
songjindian <songjindian1@huawei.com>
2023-05-26 16:25:31 +0800

    napi整改
    Signed-off-by: songjindian <songjindian1@huawei.com>

--- frameworks/js/napi/featureAbility/napi_data_ability_helper.cpp
+++ frameworks/js/napi/featureAbility/napi_data_ability_helper.cpp
@@ -1120,6 +1120,8 @@ void NAPIDataAbilityObserver::CallJsMethod()
         isCallingback_ = true;
     }
     napi_value result[ARGS_TWO] = {nullptr};
+    napi_handle_scope scope;
+    napi_open_handle_scope(env_, &scope);
     result[PARAM0] = GetCallbackErrorValue(env_, NO_ERROR);
     napi_value callback = nullptr;
     napi_value undefined = nullptr;
@@ -1138,6 +1140,7 @@ void NAPIDataAbilityObserver::CallJsMethod()
         }
         isCallingback_ = false;
     }
+    napi_close_handle_scope(env_, scope);
 }
 
 void NAPIDataAbilityObserver::OnChange()
