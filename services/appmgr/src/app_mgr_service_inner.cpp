0dd47ababc8b6127f59960a824513d23134bbd7f
handf0830 <handf0830@thundersoft.com>
2023-06-02 10:51:24 +0800

    test only
    Signed-off-by: handf0830 <handf0830@thundersoft.com>

--- services/appmgr/src/app_mgr_service_inner.cpp
+++ services/appmgr/src/app_mgr_service_inner.cpp
@@ -1666,15 +1666,15 @@ void AppMgrServiceInner::StartProcess(const std::string &appName, const std::str
         if (result != Security::AccessToken::PERMISSION_GRANTED) {
             setAllowInternet = 1;
             allowInternet = 0;
-            HILOG_ERROR("not Set netmanager group");
+            // HILOG_ERROR("not Set netmanager group");
 #ifdef APP_MGR_SERVICE_APPMS
             auto ret = SetInternetPermission(bundleInfo.uid, 0);
             HILOG_DEBUG("SetInternetPermission, ret = %{public}d", ret);
         } else {
             auto ret = SetInternetPermission(bundleInfo.uid, 1);
             HILOG_DEBUG("SetInternetPermission, ret = %{public}d", ret);
-            HILOG_ERROR("Set netmanager group");
-            bundleInfo.gids.push_back(1099);
+            // HILOG_ERROR("Set netmanager group");
+            // bundleInfo.gids.push_back(1099);
 #endif
         }
 
