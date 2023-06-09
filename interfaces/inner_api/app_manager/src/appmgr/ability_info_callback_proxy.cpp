6c64928fabfa070ad5b79048de8b4fa2940aa86a
gongyuechen <gongyuechen@huawei.com>
2023-06-09 07:22:04 +0000

    add nullptr judge
    Signed-off-by: gongyuechen <gongyuechen@huawei.com>

--- interfaces/inner_api/app_manager/src/appmgr/ability_info_callback_proxy.cpp
+++ interfaces/inner_api/app_manager/src/appmgr/ability_info_callback_proxy.cpp
@@ -86,10 +86,26 @@ void AbilityInfoCallbackProxy::NotifyStartSpecifiedAbility(const sptr<IRemoteObj
     if (!WriteInterfaceToken(data)) {
         return;
     }
-
-    data.WriteRemoteObject(callerToken);
-    data.WriteParcelable(&want);
-    data.WriteInt32(requestCode);
+    
+    if (callerToken) {
+        if (!data.WriteBool(true) || !data.WriteRemoteObject(callerToken)) {
+            HILOG_ERROR("flag and callerToken write failed.");
+            return;
+        }
+    } else {
+        if (!data.WriteBool(false)) {
+            HILOG_ERROR("flag write failed.");
+            return;
+        }
+    }
+    if (!data.WriteParcelable(&want)) {
+        HILOG_ERROR("want write failed.");
+        return;
+    }
+    if (!data.WriteInt32(requestCode)) {
+        HILOG_ERROR("%{public}s, requestCode write failed.", __func__);
+        return;
+    }
     sptr<IRemoteObject> remote = Remote();
     if (remote == nullptr) {
         HILOG_ERROR("Remote() is NULL");
