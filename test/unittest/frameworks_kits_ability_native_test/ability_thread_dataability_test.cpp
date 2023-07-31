9a7b355707ef11ea8f2e05b0129c434e2ea75015
xinking129 <xinxin13@huawei.com>
2023-07-31 15:58:25 +0800

    famode_mockabilitythreadtest.h
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- test/unittest/frameworks_kits_ability_native_test/ability_thread_dataability_test.cpp
+++ test/unittest/frameworks_kits_ability_native_test/ability_thread_dataability_test.cpp
@@ -28,7 +28,7 @@
 #include "hilog_wrapper.h"
 #include "mock_ability_impl.h"
 #include "mock_ability_lifecycle_callbacks.h"
-#include "mock_ability_thread.h"
+// #include "mock_ability_thread.h"
 #include "mock_ability_token.h"
 #include "mock_data_ability.h"
 #include "mock_page_ability.h"
