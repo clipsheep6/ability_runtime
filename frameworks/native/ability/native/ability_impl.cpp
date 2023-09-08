f51e25b86854f4bafa8fb1de547a302aef9430ac
xinking129 <xinxin13@huawei.com>
2023-09-08 10:13:15 +0800

    commit
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- frameworks/native/ability/native/ability_impl.cpp
+++ frameworks/native/ability/native/ability_impl.cpp
@@ -626,7 +626,7 @@ void AbilityImpl::AfterFocusedCommon(bool isFocused)
 void AbilityImpl::WindowLifeCycleImpl::AfterForeground()
 {
     HITRACE_METER_NAME(HITRACE_TAG_ABILITY_MANAGER, __PRETTY_FUNCTION__);
-    HILOG_DEBUG("%{public}s begin.", __func__);
+    HILOG_INFO("Call.");
     auto owner = owner_.lock();
     if (owner == nullptr || !owner->IsStageBasedModel()) {
         HILOG_ERROR("Not stage mode ability or abilityImpl is nullptr.");
@@ -655,9 +655,10 @@ void AbilityImpl::WindowLifeCycleImpl::AfterForeground()
 void AbilityImpl::WindowLifeCycleImpl::AfterBackground()
 {
     HITRACE_METER_NAME(HITRACE_TAG_ABILITY_MANAGER, __PRETTY_FUNCTION__);
-    HILOG_DEBUG("%{public}s begin.", __func__);
+    HILOG_INFO("Call.");
     auto owner = owner_.lock();
     if (owner && !owner->IsStageBasedModel()) {
+        HILOG_WARN("Not stage.");
         return;
     }
 
--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -67,6 +67,7 @@ enum class ServiceRunningState { STATE_NOT_START, STATE_RUNNING };
 constexpr int32_t BASE_USER_RANGE = 200000;
 constexpr int32_t U0_USER_ID = 0;
 constexpr int32_t INVALID_USER_ID = -1;
+constexpr int32_t SHARE_PICKER_SUCCESS = 2;
 using OHOS::AppExecFwk::IAbilityController;
 class PendingWantManager;
 /**
@@ -180,6 +181,42 @@ public:
         const sptr<IRemoteObject> &callerToken,
         int32_t userId = DEFAULT_INVAL_VALUE,
         int requestCode = DEFAULT_INVAL_VALUE) override;
+
+    /**
+     * Start ui session ability with extension session info, send session info to ability manager service.
+     *
+     * @param want, the want of the ability to start.
+     * @param callerToken, caller ability token.
+     * @param sessionInfo the information of UIExtensionContentSession.
+     * @param userId, Designation User ID.
+     * @param requestCode, Ability request code.
+     * @return Returns ERR_OK on success, others on failure.
+     */
+    virtual int StartAbilityByUIContentSession(
+        const Want &want,
+        const sptr<IRemoteObject> &callerToken,
+        const sptr<SessionInfo> &sessionInfo,
+        int32_t userId,
+        int requestCode) override;
+
+    /**
+     * Start ui session ability with extension session info, send session info to ability manager service.
+     *
+     * @param want, the want of the ability to start.
+     * @param startOptions Indicates the options used to start.
+     * @param callerToken, caller ability token.
+     * @param sessionInfo the information of UIExtensionContentSession.
+     * @param userId, Designation User ID.
+     * @param requestCode the resultCode of the ability to start.
+     * @return Returns ERR_OK on success, others on failure.
+     */
+    virtual int StartAbilityByUIContentSession(
+        const Want &want,
+        const StartOptions &startOptions,
+        const sptr<IRemoteObject> &callerToken,
+        const sptr<SessionInfo> &sessionInfo,
+        int32_t userId,
+        int requestCode) override;
 
     /**
      * Start extension ability with want, send want to ability manager service.
@@ -337,7 +374,8 @@ public:
         const sptr<IAbilityConnection> &connect,
         const sptr<IRemoteObject> &callerToken,
         AppExecFwk::ExtensionAbilityType extensionType,
-        int32_t userId = DEFAULT_INVAL_VALUE) override;
+        int32_t userId = DEFAULT_INVAL_VALUE,
+        bool isQueryExtensionOnly = false) override;
 
     virtual int ConnectUIExtensionAbility(
         const Want &want,
@@ -729,6 +767,13 @@ public:
         int32_t userId = DEFAULT_INVAL_VALUE,
         bool isStartAsCaller = false);
 
+    int StartExtensionAbilityInner(
+        const Want &want,
+        const sptr<IRemoteObject> &callerToken,
+        int32_t userId,
+        AppExecFwk::ExtensionAbilityType extensionType,
+        bool checkSystemCaller = true);
+
     int StartAbilityForOptionWrap(
         const Want &want,
         const StartOptions &startOptions,
@@ -1031,7 +1076,7 @@ public:
      * @return Returns ERR_OK on success, others on failure.
      */
     int32_t IsValidMissionIds(
-        const std::vector<int32_t> &missionIds, std::vector<MissionVaildResult> &results) override;
+        const std::vector<int32_t> &missionIds, std::vector<MissionValidResult> &results) override;
 
     virtual int32_t RequestDialogService(const Want &want, const sptr<IRemoteObject> &callerToken) override;
 
@@ -1281,7 +1326,8 @@ private:
         const sptr<IAbilityConnection> &connect,
         const sptr<IRemoteObject> &callerToken,
         AppExecFwk::ExtensionAbilityType extensionType,
-        const sptr<SessionInfo> &sessionInfo = nullptr);
+        const sptr<SessionInfo> &sessionInfo = nullptr,
+        bool isQueryExtensionOnly = false);
     int DisconnectLocalAbility(const sptr<IAbilityConnection> &connect);
     int ConnectRemoteAbility(Want &want, const sptr<IRemoteObject> &callerToken, const sptr<IRemoteObject> &connect);
     int DisconnectRemoteAbility(const sptr<IRemoteObject> &connect);
