0e71e823dae5a5629fb22a309de573d7ce714ba3
吴鸿 <wh584221@163.com>
2024-01-31 17:06:47 +0800

    IssueNo: #I8ZJU3:add extension type: sysPicker/PhotoEditor and photoEditor Description:add type sysPicker/PhotoEditor and photoEditor Sig:SIG_ApplicaitonFramework Feature or Bugfix:Feature Binary Source:No
    Signed-off-by: 吴鸿 <wh584221@163.com>

--- services/common/include/ui_extension_utils.h
+++ services/common/include/ui_extension_utils.h
@@ -48,7 +48,7 @@ const std::unordered_set<AppExecFwk::ExtensionAbilityType> UI_EXTENSION_SET = {
     AppExecFwk::ExtensionAbilityType::SYSPICKER_PHOTOPICKER,
     AppExecFwk::ExtensionAbilityType::SYS_COMMON_UI,
     AppExecFwk::ExtensionAbilityType::SYSPICKER_PHOTOEDITOR,
-    AppExecFwk::ExtensionAbilityType::PHOTOEDITOR
+    AppExecFwk::ExtensionAbilityType::PHOTO_EDITOR
 };
 const int EDM_SA_UID = 3057;
 
