9f6a9d8e5d05392350ab3ee5012e056a1b61f694
Andrey Ovsyhno <ovsyhno.andrey@huawei-partners.com>
2024-03-22 12:13:07 +0300

    GetSrcPath duplicate in ability_runtime
    Signed-off-by: Andrey Ovsyhno <ovsyhno.andrey@huawei-partners.com>
    Change-Id: I6ead473820eec094dcecf94681ba26d65f46df6f

--- frameworks/native/ability/native/js_service_extension.cpp
+++ frameworks/native/ability/native/js_service_extension.cpp
@@ -543,23 +543,7 @@ napi_value JsServiceExtension::CallObjectMethod(const char* name, napi_value con
 void JsServiceExtension::GetSrcPath(std::string &srcPath)
 {
     TAG_LOGD(AAFwkTag::SERVICE_EXT, "GetSrcPath start.");
-    if (!Extension::abilityInfo_->isModuleJson) {
-        /* temporary compatibility api8 + config.json */
-        srcPath.append(Extension::abilityInfo_->package);
-        srcPath.append("/assets/js/");
-        if (!Extension::abilityInfo_->srcPath.empty()) {
-            srcPath.append(Extension::abilityInfo_->srcPath);
-        }
-        srcPath.append("/").append(Extension::abilityInfo_->name).append(".abc");
-        return;
-    }
-
-    if (!Extension::abilityInfo_->srcEntrance.empty()) {
-        srcPath.append(Extension::abilityInfo_->moduleName + "/");
-        srcPath.append(Extension::abilityInfo_->srcEntrance);
-        srcPath.erase(srcPath.rfind('.'));
-        srcPath.append(".abc");
-    }
+    GetSrcPathBase(srcPath);
 }
 
 napi_value JsServiceExtension::CallOnConnect(const AAFwk::Want &want)
