b07b3b42236864b266bb0d2313b92d309daae3af
xinking129 <xinxin13@huawei.com>
2024-03-23 15:05:07 +0800

    整改interfaces/inner_api hilog
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- frameworks/native/runtime/ohos_loop_handler.cpp
+++ frameworks/native/runtime/ohos_loop_handler.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,19 +19,19 @@ namespace AbilityRuntime {
 
 void OHOSLoopHandler::OnReadable(int32_t)
 {
-    HILOG_DEBUG("OHOSLoopHandler::OnReadable is triggered");
+    TAG_LOGD(AAFwkTag::JSRUNTIME, "OHOSLoopHandler::OnReadable is triggered");
     OnTriggered();
 }
 
 void OHOSLoopHandler::OnWritable(int32_t)
 {
-    HILOG_DEBUG("OHOSLoopHandler::OnWritable is triggered");
+    TAG_LOGD(AAFwkTag::JSRUNTIME, "OHOSLoopHandler::OnWritable is triggered");
     OnTriggered();
 }
 
 void OHOSLoopHandler::OnTriggered()
 {
-    HILOG_DEBUG("OHOSLoopHandler::OnTriggered is triggered");
+    TAG_LOGD(AAFwkTag::JSRUNTIME, "OHOSLoopHandler::OnTriggered is triggered");
 
     uv_run(uvLoop_, UV_RUN_NOWAIT);
 
