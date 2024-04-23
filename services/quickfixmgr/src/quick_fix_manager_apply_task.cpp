48c71865b6dd8b60068d44c4409292dcd6eee60c
xinking129 <xinxin13@huawei.com>
2024-04-23 10:01:35 +0800

    fix build warning
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- services/quickfixmgr/src/quick_fix_manager_apply_task.cpp
+++ services/quickfixmgr/src/quick_fix_manager_apply_task.cpp
@@ -178,7 +178,7 @@ public:
     explicit RevokeQuickFixTaskCallback(std::shared_ptr<QuickFixManagerApplyTask> applyTask)
         : QuickFixManagerStatusCallback(applyTask)
     {}
-    virtual ~RevokeQuickFixTaskCallback() = default;
+    ~RevokeQuickFixTaskCallback() override {};
 
     void OnPatchDeployed(const std::shared_ptr<AppExecFwk::QuickFixResult> &result) override
     {
@@ -294,7 +294,7 @@ public:
         : QuickFixNotifyCallback(applyTask)
     {}
 
-    virtual ~RevokeQuickFixNotifyCallback() = default;
+    ~RevokeQuickFixNotifyCallback() override {};
 
     void OnLoadPatchDone(int32_t resultCode, [[maybe_unused]] int32_t recordId) override
     {
