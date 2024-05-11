8c32fa9a9f98a181ce971025b52590cd3c2f02e0
grs <guoruishuai@huawei.com>
2024-05-11 14:23:08 +0800

    新增API支持创建Native子进程
    Signed-off-by: grs <guoruishuai@huawei.com>

--- test/mock/frameworks_kits_appkit_test/include/mock_app_mgr_service.h
+++ test/mock/frameworks_kits_appkit_test/include/mock_app_mgr_service.h
@@ -83,7 +83,8 @@ public:
     MOCK_METHOD1(RegisterRenderStateObserver, int32_t(const sptr<IRenderStateObserver> &observer));
     MOCK_METHOD1(UnregisterRenderStateObserver, int32_t(const sptr<IRenderStateObserver> &observer));
     MOCK_METHOD2(UpdateRenderState, int32_t(pid_t renderPid, int32_t state));
-
+    MOCK_METHOD3(StartNativeChildProcess, int32_t(const std::string &libName, int32_t childProcessCount,
+        const sptr<IRemoteObject> &callback));
     void AttachApplication(const sptr<IRemoteObject>& app)
     {
         GTEST_LOG_(INFO) << "MockAppMgrService::AttachApplication called";
