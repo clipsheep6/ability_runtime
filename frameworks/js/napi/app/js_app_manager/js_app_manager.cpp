116d1350bae028a998e1b6e852f39e479f9850c4
mashaohua7 <mashaohua7@huawei.com>
2024-05-22 16:52:38 +0800

    通过bundlename|index判断应用是否再运行中
    Signed-off-by: mashaohua7 <mashaohua7@huawei.com>

--- frameworks/js/napi/app/js_app_manager/js_app_manager.cpp
+++ frameworks/js/napi/app/js_app_manager/js_app_manager.cpp
@@ -1056,27 +1056,21 @@ private:
     napi_value OnIsApplicationRunning(napi_env env, size_t argc, napi_value *argv)
     {
         TAG_LOGD(AAFwkTag::APPMGR, "Called.");
-        if (argc < ARGC_ONE) {
-            TAG_LOGE(AAFwkTag::APPMGR, "Params not match.");
-            ThrowTooFewParametersError(env);
-            return CreateJsUndefined(env);   
-        }
-        int32_t appCloneIndex = 0;
         if (argc < ARGC_TWO) {
             TAG_LOGE(AAFwkTag::APPMGR, "Params not match.");
             ThrowTooFewParametersError(env);
             return CreateJsUndefined(env);
-
-            if (!ConvertFromJsValue(env, argv[1], appCloneIndex)) {
-                TAG_LOGE(AAFwkTag::APPMGR, "Get appCloneIndex wrong.");
-                ThrowInvalidParamError(env, "Parse param appCloneIndex failed, must be a string");
-                return CreateJsUndefined(env);
-            }
         }
         std::string bundleName;
         if (!ConvertFromJsValue(env, argv[0], bundleName)) {
             TAG_LOGE(AAFwkTag::APPMGR, "Get bundle name wrong.");
             ThrowInvalidParamError(env, "Parse param bundleName failed, must be a string");
+            return CreateJsUndefined(env);
+        }
+        int32_t appCloneIndex = 0;
+        if (argc > ARGC_ONE && !ConvertFromJsValue(env, argv[1], appCloneIndex)) {
+            TAG_LOGE(AAFwkTag::APPMGR, "Get appCloneIndex wrong.");
+            ThrowInvalidParamError(env, "Parse param appCloneIndex failed, must be a string");
             return CreateJsUndefined(env);
         }
 
