f0e400c070f4a1fc77b010552095d64742b2d071
sunxuhui_wangzhibo <sunxuhui7@huawei.com>
2024-06-14 16:37:23 +0800

    代码整改
    Signed-off-by: sunxuhui_wangzhibo <sunxuhui7@huawei.com>

--- tools/aa/include/ability_tool_command.h
+++ tools/aa/include/ability_tool_command.h
@@ -16,6 +16,7 @@
 #ifndef OHOS_ABILITY_RUNTIME_ABILITY_TOOL_COMMAND_H
 #define OHOS_ABILITY_RUNTIME_ABILITY_TOOL_COMMAND_H
 
+#include <getopt.h>
 #include "ability_command.h"
 #include "shell_command.h"
 #include "start_options.h"
@@ -39,12 +40,29 @@ private:
     ErrCode RunAsForceStop();
     ErrCode RunAsTestCommand();
 
+    bool ParseStartAbilityArgsOtherOption(int optind, std::string& paramName, std::string& paramValue, std::smatch& sm,
+        int32_t& windowMode);
+    bool ParseStartAbilityArgsFlags(std::string& paramValue, std::smatch& sm, int& flags);
+    ErrCode ParseStartAbilityArgsParameterCheck(const std::string& abilityName, const std::string& bundleName);
+    void ParseStartAbilityArgsSetWant(bool isColdStart, bool isDebugApp, int flags, Want& want);
+    ErrCode ParseStartAbilityArgsSetStartOptions(StartOptions& startOptions, int32_t windowMode);
     ErrCode ParseStartAbilityArgsFromCmd(Want& want, StartOptions& startOptions);
     ErrCode ParseStopServiceArgsFromCmd(Want& want);
     ErrCode ParseTestArgsFromCmd(std::map<std::string, std::string>& params);
     bool GetKeyAndValueByOpt(int optind, std::string& key, std::string& value);
 
     std::shared_ptr<AbilityManagerShellCommand> aaShellCmd_;
+    struct option startAbilityLongOptions[9] = {
+        {"help", no_argument, nullptr, 'h'},
+        {"device", required_argument, nullptr, 'd'},
+        {"ability", required_argument, nullptr, 'a'},
+        {"bundle", required_argument, nullptr, 'b'},
+        {"options", required_argument, nullptr, 'o'},
+        {"flags", required_argument, nullptr, 'f'},
+        {"cold-start", no_argument, nullptr, 'C'},
+        {"debug", no_argument, nullptr, 'D'},
+        {nullptr, 0, nullptr, 0},
+    };
 };
 } // namespace AAFwk
 } // namespace OHOS
