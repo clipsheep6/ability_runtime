63169733d3546726f4f6b2ca649364ee4ccff737
sunxuhui <sunxuhui7@huawei.com>
2024-06-17 15:21:48 +0800

    代码整改
    Signed-off-by: sunxuhui <sunxuhui7@huawei.com>

--- interfaces/kits/native/ability/native/ability_runtime/js_ability_context.h
+++ interfaces/kits/native/ability/native/ability_runtime/js_ability_context.h
@@ -143,7 +143,6 @@ private:
 };
 
 napi_value CreateJsAbilityContext(napi_env env, std::shared_ptr<AbilityContext> context);
-
 struct ConnectCallback {
     std::unique_ptr<NativeReference> jsConnectionObject_ = nullptr;
 };
