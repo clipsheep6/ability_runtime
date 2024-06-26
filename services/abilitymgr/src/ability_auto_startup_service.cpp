e71d65d8ec44b62b8e84282fd1a405150416a7b2
jiangzhijun8 <jiangzhijun7@huawei.com>
2024-06-26 14:36:21 +0800

    add log
    Signed-off-by: jiangzhijun8 <jiangzhijun7@huawei.com>

--- services/abilitymgr/src/ability_auto_startup_service.cpp
+++ services/abilitymgr/src/ability_auto_startup_service.cpp
@@ -482,9 +482,12 @@ bool AbilityAutoStartupService::GetBundleInfo(const std::string &bundleName,
     } else if (appIndex <= GlobalConstant::MAX_APP_CLONE_INDEX) {
         auto bundleFlag = static_cast<int32_t>(AppExecFwk::BundleFlag::GET_BUNDLE_WITH_EXTENSION_INFO);
         TAG_LOGD(AAFwkTag::AUTO_STARTUP, "appindex = : %{public}d.", appIndex);
-        int32_t bundleMgrResult = 0;
-        bundleMgrResult = IN_PROCESS_CALL(bms->GetCloneBundleInfo(bundleName, bundleFlag, appIndex, bundleInfo, userId);
+        bool bundleMgrResult;
+        int32_t errorCode = 0;
+        errorCode = bms->GetCloneBundleInfo(bundleName, bundleFlag, appIndex, bundleInfo, userId);
+        bundleMgrResult = IN_PROCESS_CALL(bms->GetCloneBundleInfo(bundleName, bundleFlag, appIndex, bundleInfo, userId));
         if (!bundleMgrResult) {
+            TAG_LOGD(AAFwkTag::AUTO_STARTUP, "errorCode = : %{public}d.", errorCode);
             TAG_LOGD(AAFwkTag::AUTO_STARTUP, "bundleMgrResult = : %{public}d.", bundleMgrResult);
             TAG_LOGW(AAFwkTag::AUTO_STARTUP, "Failed to get clone bundle info.");
             return false;
