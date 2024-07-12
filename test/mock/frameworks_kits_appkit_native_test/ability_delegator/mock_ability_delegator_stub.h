5d0981af6b7f6cf2043d5428cfcf73960d573959
zhubingwei <zhubingwei@huawei.com>
2024-07-12 13:39:45 +0800

    add AbilityWindowConfigTransitionDone
    Signed-off-by: zhubingwei <zhubingwei@huawei.com>

--- test/mock/frameworks_kits_appkit_native_test/ability_delegator/mock_ability_delegator_stub.h
+++ test/mock/frameworks_kits_appkit_native_test/ability_delegator/mock_ability_delegator_stub.h
@@ -58,6 +58,7 @@ public:
     MOCK_METHOD2(AddWindowInfo, void(const sptr<IRemoteObject>& token, int32_t windowToken));
     MOCK_METHOD2(AttachAbilityThread, int(const sptr<IAbilityScheduler>& scheduler, const sptr<IRemoteObject>& token));
     MOCK_METHOD3(AbilityTransitionDone, int(const sptr<IRemoteObject>& token, int state, const PacMap&));
+    MOCK_METHOD2(AbilityWindowConfigTransitionDone, int(const sptr<IRemoteObject>& token, const WindowConfig&));
     MOCK_METHOD2(
         ScheduleConnectAbilityDone, int(const sptr<IRemoteObject>& token, const sptr<IRemoteObject>& remoteObject));
     MOCK_METHOD1(ScheduleDisconnectAbilityDone, int(const sptr<IRemoteObject>& token));
