2d19b0956469719ff8b601018d2a8991b87debb4
xlw <xialiangwei1@huawei.com>
2024-07-25 11:29:26 +0800

    修复startserviceextension过程中再connect，want没有更新的问题
    Signed-off-by: xlw <xialiangwei1@huawei.com>
    Change-Id: I13b36d7cfe4e652746fcddafe525cf794410754b

--- services/abilitymgr/include/ability_record.h
+++ services/abilitymgr/include/ability_record.h
@@ -1000,6 +1000,12 @@ public:
 
     void SetSpecifyTokenId(const uint32_t specifyTokenId);
 
+    void SaveConnectWant(Want &want);
+
+    void UpdateConnectWant();
+
+    void RemoveConnectWant();
+
 protected:
     void SendEvent(uint32_t msg, uint32_t timeOut, int32_t param = -1);
 
@@ -1183,6 +1189,7 @@ private:
     mutable ffrt::mutex dumpLock_;
     mutable ffrt::mutex resultLock_;
     mutable ffrt::mutex wantLock_;
+    mutable ffrt::mutex connectWantLock_;
     mutable ffrt::condition_variable dumpCondition_;
     mutable bool isDumpTimeout_ = false;
     std::vector<std::string> dumpInfos_;
@@ -1219,6 +1226,8 @@ private:
 
     bool isRestartApp_ = false; // Only app calling RestartApp can be set to true
     uint32_t specifyTokenId_ = 0;
+
+    std::shared_ptr<Want> connectWant_ = nullptr;
 };
 }  // namespace AAFwk
 }  // namespace OHOS
